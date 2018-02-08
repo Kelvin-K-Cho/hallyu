3730ce6117915f1613aa89bacfbcdbb9b9c1abfc
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-02-08 11:18:26 -0800

    Drop Likes Table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180206171311) do
+ActiveRecord::Schema.define(version: 20180208191516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,18 +31,6 @@ ActiveRecord::Schema.define(version: 20180206171311) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["band_name"], name: "index_bands_on_band_name"
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "value", null: false
-    t.string "likeable_type", null: false
-    t.integer "likeable_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["likeable_type", "likeable_id"], name: "index_likes_on_likeable_type_and_likeable_id"
-    t.index ["user_id", "likeable_type", "likeable_id"], name: "index_likes_on_user_id_and_likeable_type_and_likeable_id", unique: true
-    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "stations", force: :cascade do |t|
