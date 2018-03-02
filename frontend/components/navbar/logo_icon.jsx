074db06904550720463243f0b081ec696220bbf9
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-03-02 10:21:22 -0800

    Updated logo

--- frontend/components/navbar/logo_icon.jsx
+++ frontend/components/navbar/logo_icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LogoIcon = () => (
   <div id="container-logo">
-    <img id="img-logo" src="https://s3-us-west-1.amazonaws.com/hallyu-dev/images/151993105952239162.png"/>
+    <img id="img-logo" src="https://s3-us-west-1.amazonaws.com/hallyu-dev/images/151993105952239162-1.png"/>
   </div>
 );
 
