2f4caf1c28c35d979ed0a721e43b8059e7435149
Kelvin Cho <cho.k.kelvin@gmail.com>
2018-03-05 09:09:37 -0800

    Update loading logo

--- frontend/components/station/loading_icon.jsx
+++ frontend/components/station/loading_icon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingIcon = () => (
   <div id="container-loading">
-    <img id="img-loading" src="https://i.imgur.com/VG0uxec.png"/>
+    <img id="img-loading" src="https://s3-us-west-1.amazonaws.com/hallyu-dev/images/large_1_1600x900.jpg"/>
   </div>
 );
 
